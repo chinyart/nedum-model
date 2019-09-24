984b127a37380f3e24297455011838e1f5c18fd8
unor_chinedum_chukwuenewanta <chinedum.chukwuenewanta@vidicon.ng>
2019-09-24 11:54:41 +0100

    updated the displayname func

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -2,7 +2,7 @@ package controllers
 
 import "github.com/chinyart/nedum-model/models"
 
-//DisplayPerson to display person details
+//DisplayPersonName to display person details
 func DisplayPersonName(personBio models.PersonBio) (string, error) {
 	//personBio.Name = "chidi"
 	return personBio.Name, nil
